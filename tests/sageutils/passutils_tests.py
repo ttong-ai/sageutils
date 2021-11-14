2677f028fcb8216af0f83f00fb5b36664520bf34
Tony Tong <tao.tong@auditoria.ai>
2021-11-14 14:52:26 -0800

    Add fire command line interface

--- tests/sageutils/passutils_tests.py
+++ tests/sageutils/passutils_tests.py
@@ -3,11 +3,11 @@ from sageutils.passutils import *
 
 def test_permutate():
     assert (
-        permutate("one two three four five six seven eight nine ten")
+        Permutater.permutate("one two three four five six seven eight nine ten")
         == "one four ten five eight three six seven two nine"
     )
     assert (
-        reverse_permutate("one four ten five eight three six seven two nine")
+        Permutater.reverse_permutate("one four ten five eight three six seven two nine")
         == "one two three four five six seven eight nine ten"
     )
     print("Permutation tests passed")
