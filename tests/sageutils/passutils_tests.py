46f9ac0c027bc817e75b6baa3fbdea342e43f094
Tony Tong <tao.tong@auditoria.ai>
2021-11-14 14:27:49 -0800

    Add unit test

---
+++ tests/sageutils/passutils_tests.py
@@ -0,0 +1,12 @@
+from sageutils.passutils import *
+
+def test_permutate():
+    assert (
+        permutate("one two three four five six seven eight nine ten")
+        == "one four ten five eight three six seven two nine"
+    )
+    assert (
+        reverse_permutate("one four ten five eight three six seven two nine")
+        == "one two three four five six seven eight nine ten"
+    )
+    print("Permutation tests passed")
