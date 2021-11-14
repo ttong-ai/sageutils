f822cc741fd10c965847584bf453e066ce14cea7
Tony Tong <tao.tong@auditoria.ai>
2021-11-14 15:33:14 -0800

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,20 @@
+from sageutils import __version__
+from setuptools import find_packages, setup
+
+long_description = "Sage common utilities library"
+
+
+setup(
+    name="sageutils",
+    packages=find_packages(),
+    version=__version__,
+    description="Sage common utilities library",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    author="Tony Tong",
+    author_email="ttong@pro-ai.org",
+    license="MIT",
+    python_requires=">=3.8",
+    include_package_data=True,
+    install_requires=[],
+)
