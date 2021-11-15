5545d7034d7e36b750c71b77b9ebbdbff168e697
Tony Tong <tony.tong@punchh.com>
2021-11-14 23:48:33 -0800

    Update test

--- tests/sageutils/passutils_tests.py
+++ tests/sageutils/passutils_tests.py
@@ -3,11 +3,11 @@ from sageutils.passutils import *
 
 def test_permutate():
     assert (
-        Permutater.permutate("one two three four five six seven eight nine ten")
+        permutate("one two three four five six seven eight nine ten")
         == "one four ten five eight three six seven two nine"
     )
     assert (
-        Permutater.reverse_permutate("one four ten five eight three six seven two nine")
+        reverse_permutate("one four ten five eight three six seven two nine")
         == "one two three four five six seven eight nine ten"
     )
     print("Permutation tests passed")
