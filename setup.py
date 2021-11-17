bba0a6fd5d47d0e5fabf1e9956148b33a176d95e
Tony Tong <tao.tong@auditoria.ai>
2021-11-16 22:55:53 -0800

    Add setup and build file

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ long_description = "Sage common utilities library"
 setup(
     name="sageutils",
     packages=find_packages(),
-    scripts=["scripts/passutils"],
+    scripts=["scripts/passutil"],
     version=__version__,
     description="Sage common utilities library",
     long_description=long_description,
